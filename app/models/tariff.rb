0838d48efd486d4a09cc79585f7cb3cab68fdc9e
Kai-Arne Reiter <git@mirrorspock.com>
2018-02-28 12:01:19 +0100

    add scope and cleanup index

--- app/models/tariff.rb
+++ app/models/tariff.rb
@@ -1,9 +1,13 @@
 class Tariff < ApplicationRecord
   
-  def self.import(file)
+  scope :unique_countries, ->{select('DISTINCT country').map{|c| c.country}.sort}
+  # scope :unique_zip_for_country, ->{select('DISTINCT country').map{|c| c.country}.sort}
+  
+  
+  def import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Tariff.create({country: row[0], zip: row[1], weight: row[2], price: row[3], carrier: row[4], expire: row[5], transit_time: row[6], loading_region: row[7]})
     end 
-  end
+  end  
 
 end
